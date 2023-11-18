75a9c7be5c5ecd0f87ad9be64bea050f8efa7bca
“Arthur <arthur.f22@aluno.ifsc.edu.br>
2023-11-17 22:17:11 -0300

    comecei a logica do checkmate, só o bagulo de voltar o lance feito q tem q dar uma adulterada para de fato voltar o lance random q o pc fez tlgd?? mas deve ser rapidinho

--- utils.py
+++ utils.py
@@ -22,6 +22,42 @@ def check(posição_rei, peças_dic):
     else:
         return False
 
+#tem q adaptar esse bagulho de ir e voltar o tabuleiro e não sei se o Break funciona assim
+def checkmate(posição_rei, peças_dic,tab):
+    checkmate_bool=True
+    lances=[]
+    #gera todas as posições do tab
+    for x in range(tab.tamanho):
+        for y in range(tab.tamanho):
+            lances.append([x,y])
+    #pega todas as peças vivas
+    for x in tab.get_mapeamento_peças.values():
+        for y in lances:
+            if x.verifica(y):
+                rei_turno=modifica_turno(turno,"R")
+                mapeamento_backup = tab.get_mapeamento_peças()
+                destruidor(tab.get_mapeamento_peças(),tab,pos_desejada)
+                peça_movendo.set_posi(pos_atual, pos_desejada)
+                posi_rei = tab.get_cord_peça(rei_turno)
+    
+                if check(posi_rei,tab.get_mapeamento_peças()):
+                    peça = tab.get_peça_cord(pos_desejada)
+                    peça_movendo.set_posi(pos_desejada, pos_atual)
+                    tab.set_estado({peça:pos_atual})
+                    tab.mapeamento_recover(mapeamento_backup)
+                    print('Seu rei está em cheque!')
+                    print('Lance inválido.')
+
+                else:
+                    checkmate_bool=False
+                    peça = tab.get_peça_cord(pos_desejada)
+                    peça_movendo.set_posi(pos_desejada, pos_atual)
+                    tab.set_estado({peça:pos_atual})
+                    tab.mapeamento_recover(mapeamento_backup)
+                    break
+
+
+
 # def checkmate(tabuleiro, turno, dicionario):
 #     tabuleiro2=tabuleiro
 #     for x in dicionario(values):
